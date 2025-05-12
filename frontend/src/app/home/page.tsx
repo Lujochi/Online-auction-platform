7b7673f3676d8b538524615e262da13ae0519ae5
Luan Chiodini <a.luanjc@gmail.com>
2025-05-12 15:20:12 -0300

    Adicionei 2 paragrafos

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -3,6 +3,8 @@ const HomePage = () => {
     <div>
       <h1>Welcome to the Online Auction Platform</h1>
       <p>Browse and bid on a variety of items from the comfort of your home.</p>
+      <p>Sign up to start bidding or list your own items for auction!</p>
+      <p>Check out our latest auctions and find great deals!</p>
     </div>
   );
 };
